2b4a8e91c06eca878ce374a0fd9bda9dab224822
moudome <achimmoundomesalom@gmail.com>
2025-04-05 02:20:19 +0200

    premier commit

---
+++ src/components/Technologie.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { FaHtml5, FaCss3 } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { DiMysql } from "react-icons/di";
+import { MdFlutterDash } from "react-icons/md";
+import { motion } from "motion/react";
+import { animate } from "motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+const Technologie = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <MdFlutterDash className="text-7xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-red-700" />
+        </motion.div>{" "}
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-500 " />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <DiMysql className="text-7xl text-sky-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaHtml5 className="text-7xl text-sky-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaCss3 className="text-7xl text-sky-500" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologie;
---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "../assets/projects/project1.jpg";
+import project3 from "../assets/projects/project3.jpg";
+import project4 from "../assets/projects/project4.jpg";
+
+export const HERO_CONTENT = `Je suis un développeur web et mobile frontend passionné, doté d'un talent pour la création d'applications web et mobile robustes et évolutives. Fort de 3 ans d'expérience pratique, j'ai perfectionné mes compétences dans les technologies front-end comme React js ,flutter et ainsi que dans les technologies  MySQL et MongoDB. Mon objectif est de mettre à profit mon expertise pour créer des solutions innovantes qui stimulent la croissance des entreprises et offrent des expériences utilisateur exceptionnelles.`;
+
+export const ABOUT_TEXT = `Je suis un développeur web et mobile frontend dévoué et polyvalent, passionné par la création d'applications web et mobile efficaces et conviviales. Avec 3 ans d'expérience personnelle, j'ai travaillé avec diverses technologies, notamment le framwork React js, MySQL, MongoDB et flutter pour le mobile. Mon parcours dans le développement web et mobile a commencé par une profonde curiosité pour le fonctionnement des choses, et il s'est transformé en une carrière où je m'efforce continuellement d'apprendre et de m'adapter à de nouveaux défis. Je m'épanouis dans les environnements collaboratifs et j'aime résoudre des problèmes complexes pour fournir des solutions de haute qualité. En dehors du codage, j'aime rester actif, explorer de nouvelles technologies et contribuer à des projets open source.`;
+
+export const EXPERIENCES = [
+  {
+    year: "20/09/2024 - 01/10/2024",
+    role: "Conception d une application web frontend d Ecomerce qui a les fonctions suivantes ",
+    company: "",
+    description: `L'authentification,
+Visites les articles par categories
+,Mettre dans le pagnier
+,Mode de payement,
+Responsive sur tout les ecrans.`,
+    technologies: ["HTML", "CSS", "React-js", "tailwindcss"],
+  },
+  {
+    year: " 02/11/2024 - 07/10/2024",
+    role: "MON PORTOFOLIO",
+    company: "Adobe",
+    description: `Mon protofolio qui regroupe, mes traveaux personnel ,mes contacts,les technolgies que j'utilisent,mes experiences`,
+    technologies: ["HTML", "CSS", "React-js", "tailwindcss"],
+  },
+  {
+    year: "10/11/2024 - 10/01/2025",
+    role: "Conception d'une application web frontend de recherches d'appartement meuble  qui a les fonctions suivantes:",
+    company: "",
+    description: `L'authentification
+,Visites les appartements
+,Lire les temoignages
+,Prendre contact avec l imobilier
+,Responsive sur tout les ecrans
+,Envoyez un message par email
+,Mode sombre et claire`,
+    technologies: ["HTML", "CSS", "React-js", "tailwindcss"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "l application est dote des fonctionalite comme la liste des produits, mettre dans le pagnirer, et l utilisation d authentification.",
+    technologies: ["HTML", "CSS", "React", "tailwindcss"],
+    liveLink: "https://davishopp-achim.vercel.app/",
+  },
+
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "Un site web portfolio personnel présentant des projets, des compétences et des informations de contact.",
+    technologies: ["HTML", "CSS", "React", "taillwincss"],
+    liveLink: "URL_DU_PROJET_EN_LIGNE_1",
+  },
+  {
+    title: "Recherche d appartement a louer",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "React", "taillwincss"],
+    liveLink: "https://achim-moudome-hotel.vercel.app/",
+  },
+];
+
+export const CONTACT = {
+  address: "Yelwa, Garoua,  ",
+  phoneNo: "+237 697647717 ",
+  email: "achimmoundomesalom@gmail.com",
+};
